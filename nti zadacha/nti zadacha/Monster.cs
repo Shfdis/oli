c23d494d396d85a3f3c30fe0a533328bd50019a3
TheBranchPuzzle <77299316+TheBranchPuzzle@users.noreply.github.com>
2021-11-20 18:58:34 +0300

    add project :+1:

---
+++ nti zadacha/nti zadacha/Monster.cs
@@ -0,0 +1,17 @@
+﻿namespace nti_zadacha
+{
+    public class Monster
+    {
+        public int Damage { get; set; }
+
+        public int HP { get; set; }
+
+        public Monster(int damage, int hp)
+        {
+            Damage = damage;
+            HP = hp;
+        }
+
+
+    }
+}
